ea916927a3e960d0138f95c81348f0f8e2024167
Nicolas Blanchard <blanchard.nicolas@hotmail.com>
2022-11-07 21:07:36 +0100

    Add search basic

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,30 @@
+class MoviesController < ApplicationController
+  def index
+    if params[:query].present?
+      # @movies = Movie.where(title: params[:query])
+      # ------------------------------
+      # @movies = Movie.where("title ILIKE ?", "%#{params[:query]}%")
+      # ------------------------------
+      # sql_query = "title ILIKE :query OR synopsis ILIKE :query"
+      # @movies = Movie.where(sql_query, query: "%#{params[:query]}%")
+      # ------------------------------
+      # sql_query = <<~SQL
+      #   movies.title ILIKE :query
+      #   OR movies.synopsis ILIKE :query
+      #   OR directors.first_name ILIKE :query
+      #   OR directors.last_name ILIKE :query
+      # SQL
+      # @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
+      # -------------------------------
+      sql_query = <<~SQL
+        movies.title @@ :query
+        OR movies.synopsis @@ :query
+        OR directors.first_name @@ :query
+        OR directors.last_name @@ :query
+      SQL
+      @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
+    else
+      @movies = Movie.all
+    end
+  end
+end
